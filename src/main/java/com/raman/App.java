c7bc32bbde33441ad4c55aa9fddaf4d84d7dfd64
jimoj <jaimelega4@gmail.com>
2023-07-11 12:53:27 +0200

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Allianz Maven plugin test with docker!!!!" );
+        System.out.println( "Welcome to Allianz Maven plugin test with docker !!!!" );
     }
 }
